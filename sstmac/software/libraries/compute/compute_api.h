863d1df8fbf1f830b2fb85d4f16b6f361883295f
jjwilke <jjwilke@sandia.gov>
2017-02-09 14:36:59 -0800

    some fixes to compute interface, docs, tests

--- sstmac/software/libraries/compute/compute_api.h
+++ sstmac/software/libraries/compute/compute_api.h
@@ -5,14 +5,98 @@
 extern "C" {
 #endif
 
+/**
+ * @brief sstmac_sleep SST virtual equivalent of Linux sleep
+ * @param secs
+ * @return Always zero, successful return code for Linux
+ */
 int
 sstmac_sleep(unsigned int secs);
 
+/**
+ * @brief sstmac_usleep SST virtual equivalent of Linux usleep
+ * @param usecs
+ * @return Always zero, successful return code for Linux
+ */
 int
 sstmac_usleep(unsigned int usecs);
 
+/**
+ * @brief sstmac_compute Compute for a specified number of seconds
+ * @param secs
+ */
 void
 sstmac_compute(double secs);
+
+/**
+ * @brief sstmac_compute_detailed Model a specific compute block
+ * @param nflops  The number of flops executed in the compute block
+ * @param nintops The number of int ops executed in the compute block
+ *
+ * @param bytes
+ */
+void
+sstmac_compute_detailed(long nflops, long nintops, long bytes);
+
+/**
+ * @brief sstmac_compute_loop
+ * @param num_loops        The number of loops to execute
+ * @param nflops_per_loop  The number of flops per loop in the inner loop
+ * @param nintops_per_loop The number of integer ops in the inner loop (not including loop predicates like i < N)
+ * @param bytes_per_loop   The average number of unique bytes read + written per loop
+ */
+void
+sstmac_compute_loop(long num_loops,
+                    int nflops_per_loop,
+                    int nintops_per_loop,
+                    int bytes_per_loop);
+
+/**
+ * @brief sstmac_compute_loop2
+ * @param isize           The number of indices in the outer loop (imax - imin)
+ * @param jsize           The number of indices in the inner loop (jmax - jmin)
+ * @param nflops_per_loop  The number of flops per loop in the inner loop
+ * @param nintops_per_loop The number of integer ops in the inner loop (not including loop predicates like i < N)
+ * @param bytes_per_loop   The average number of unique bytes read + written per loop
+ */
+void
+sstmac_compute_loop2(long isize, long jsize,
+                    int nflops_per_loop,
+                    int nintops_per_loop,
+                    int bytes_per_loop);
+
+/**
+ * @brief sstmac_compute_loop3
+ * @param isize           The number of indices in the outer loop (imax - imin)
+ * @param jsize           The number of indices in the inner loop (jmax - jmin)
+ * @param ksize           The number of indices in the inner loop (kmax - kmin)
+ * @param nflops_per_loop  The number of flops per loop in the inner loop
+ * @param nintops_per_loop The number of integer ops in the inner loop (not including loop predicates like i < N)
+ * @param bytes_per_loop   The average number of unique bytes read + written per loop
+ */
+void
+sstmac_compute_loop3(long isize, long jsize,
+                    long ksize,
+                    int nflops_per_loop,
+                    int nintops_per_loop,
+                    int bytes_per_loop);
+
+/**
+ * @brief sstmac_compute_loop4
+ * @param isize           The number of indices in the outer loop (imax - imin)
+ * @param jsize           The number of indices in the inner loop (jmax - jmin)
+ * @param ksize           The number of indices in the inner loop (kmax - kmin)
+ * @param lsize           The number of indices in the inner loop (lmax - lmin)
+ * @param nflops_per_loop  The number of flops per loop in the inner loop
+ * @param nintops_per_loop The number of integer ops in the inner loop (not including loop predicates like i < N)
+ * @param bytes_per_loop   The average number of unique bytes read + written per loop
+ */
+void
+sstmac_compute_loop4(long isize, long jsize,
+                    long ksize, long lsize,
+                    int nflops_per_loop,
+                    int nintops_per_loop,
+                    int bytes_per_loop);
 
 void
 sstmac_memread(long bytes);
@@ -22,35 +106,6 @@ sstmac_memwrite(long bytes);
 
 void
 sstmac_memcpy(long bytes);
-
-void
-sstmac_compute_detailed(long nflops, long nintops, long bytes);
-
-void
-sstmac_compute_loop(long num_loops,
-                    int nflops_per_loop,
-                    int nintops_per_loop,
-                    int bytes_per_loop);
-
-void
-sstmac_compute_loop2(long isize, long jsize,
-                    int nflops_per_loop,
-                    int nintops_per_loop,
-                    int bytes_per_loop);
-
-void
-sstmac_compute_loop3(long isize, long jsize,
-                    long ksize,
-                    int nflops_per_loop,
-                    int nintops_per_loop,
-                    int bytes_per_loop);
-
-void
-sstmac_compute_loop4(long isize, long jsize,
-                    long ksize, long lsize,
-                    int nflops_per_loop,
-                    int nintops_per_loop,
-                    int bytes_per_loop);
 
 #define SSTMAC_sleep(...) sstmac_sleep(__VA_ARGS__)
 #define SSTMAC_usleep(...) sstmac_usleep(__VA_ARGS__)
