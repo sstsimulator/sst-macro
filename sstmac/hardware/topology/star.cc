39b2a95f04a727117f2f694dcec8b1ebf63cf50c
Joseph P. Kenny <jpkenny@s998558ca.ca.sandia.gov>
2017-08-15 11:02:17 -0700

    star topology is functional and tested by make check

---
+++ sstmac/hardware/topology/star.cc
@@ -0,0 +1,93 @@
+/**
+Copyright 2009-2017 National Technology and Engineering Solutions of Sandia, 
+LLC (NTESS).  Under the terms of Contract DE-NA-0003525, the U.S.  Government 
+retains certain rights in this software.
+
+Sandia National Laboratories is a multimission laboratory managed and operated
+by National Technology and Engineering Solutions of Sandia, LLC., a wholly 
+owned subsidiary of Honeywell International, Inc., for the U.S. Department of 
+Energy's National Nuclear Security Administration under contract DE-NA0003525.
+
+Copyright (c) 2009-2017, NTESS
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Sandia Corporation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Questions? Contact sst-macro-help@sandia.gov
+*/
+
+#include <sstream>
+#include <sstmac/hardware/topology/star.h>
+#include <sprockit/sim_parameters.h>
+
+namespace sstmac {
+namespace hw {
+
+star::star(sprockit::sim_parameters* params) :
+  structured_topology(params,
+                      InitMaxPortsIntra::I_Remembered,
+                      InitGeomEjectID::I_Remembered)
+{
+  max_ports_intra_network_ = 0;
+  eject_geometric_id_ = max_ports_intra_network_;
+}
+
+void
+star::configure_vc_routing(std::map<routing::algorithm_t, int> &m) const
+{
+  m[routing::minimal] = 1;
+  m[routing::minimal_adaptive] = 1;
+  m[routing::valiant] = 1;
+  m[routing::ugal] = 1;
+}
+
+void
+star::minimal_route_to_switch(switch_id current_sw_addr,
+                                  switch_id dest_sw_addr,
+                                  routable::path &path) const
+{
+  path.vc = 0;
+  path.set_outport(dest_sw_addr);
+}
+
+void
+star::connected_outports(switch_id src, std::vector<connection>& conns) const
+{
+  conns.resize(0);
+}
+
+void
+star::configure_individual_port_params(
+    switch_id src, sprockit::sim_parameters *switch_params) const
+{
+  topology::configure_individual_port_params(0, num_switches(), switch_params);
+}
+
+}
+} //end of namespace sstmac
