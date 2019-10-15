bbff8fd43020e74767fb0e24bac362a0fdded0fc
CA Lewis <canlewi@sandia.gov>
2019-10-15 13:41:31 -0700

    Added a test for const C style arrays in a namespace.

--- tests/clang_src2src/deglobal_array.cc
+++ tests/clang_src2src/deglobal_array.cc
@@ -5,13 +5,14 @@ int xx[42];
 namespace y {
   int x[] = {1,2};
   int xx[42];
+  const int cxx[] = {1,2,3};
 }
 
 void fxn()
 {
   int* z = x;
   int* zz = y::x;
-  y::xx[0] = 0;
+  y::xx[0] = y::cxx[0];
   xx[0] = 0;
 }
 
